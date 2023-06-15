b928aa3bde23337dc51246f22c61efdb67264cd0
essaiydy <imaneessaiydy2@gmail.com>
2023-06-15 10:20:04 -0700

    allc

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return : return 0 if itwork with succes.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
