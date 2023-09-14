b5c74d8f00be786f468ec43db8b6e141a8c56aed
essaiydy <imaneessaiydy2@gmail.com>
2023-09-14 10:24:32 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * print_dlistint - print and count all element of a list.
+ * @h: element to start.
+ * Return: the number of nodes.
+ */
 size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
