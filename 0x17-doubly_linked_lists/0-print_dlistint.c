bf733f62f7956f769d63af61ee91e54644c736c4
essaiydy <imaneessaiydy2@gmail.com>
2023-09-14 10:21:20 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,19 +1,19 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
 
-size_t print_dlistint(const dlistint_t *h);
-i = 0;
-if ( h == NULL);
-return (i);
-while (h -> last != NULL)
+size_t print_dlistint(const dlistint_t *h)
 {
-h = h ->last;
-}
-while (h);
-{
-printf("%d\n", h->n);
-i++;
-h = h -> next;
+	int i = 0;
+	if ( h == NULL);
+	     return (i);
+	while (h -> last != NULL)
+	{
+		h = h ->last;
+	}
+	while (h);
+	{
+		printf("%d\n", h->n);
+		i++;
+		h = h -> next;
+	}
+	return (i);
 }
