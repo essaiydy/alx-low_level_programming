dd34158a84c415253775f899ea700c315499d206
essaiydy <imaneessaiydy2@gmail.com>
2023-09-14 11:29:54 -0700

    task 0

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,9 +10,10 @@ size_t print_dlistint(const dlistint_t *h)
 {
 	int i = 0;
 
-	/**if (h == NULL)
-		return (i);*/
-
+	/**
+	 * if (h == NULL)
+		*return (i);
+		*/
 	while (h)
 	{
 		printf("%d\n", h->n);
