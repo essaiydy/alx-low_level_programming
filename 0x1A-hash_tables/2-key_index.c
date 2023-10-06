2348966dafa4b287e83892ed06b327901cec5c1e
essaiydy <imaneessaiydy2@gmail.com>
2023-10-05 17:44:40 -0700

    task3

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,7 +8,7 @@
  * Return: the index.
  */
 
-unsigned long int key_index(const unsigned char *key, unsigned long int size);
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	return (hash_djb2(key) % size);
 }
