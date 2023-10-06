f9a0826c4423b2c036415d86970e10302db83700
essaiydy <imaneessaiydy2@gmail.com>
2023-10-05 17:43:20 -0700

    task3

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - cherche the key index whereyou need to store.
+ * @size:  is the size of the array of the hash table.
+ * @key: is the key and the str in the djb2 fonction.
+ *
+ * Return: the index.
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+{
+	return (hash_djb2(key) % size);
+}
